9ccaee898753d9747fddeaa187c02adcb2c77f93
Bacem T ouil <186514619+Bacemtouil-ZB@users.noreply.github.com>
2025-08-04 20:13:49 +0200

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import dotenv, { config } from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -9,13 +10,15 @@ import ratelimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 const app = express();
-
+const _dirname = path.resolve();
 //middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json()); //this middlware will parse json bodies => req.body
 app.use(ratelimiter);
 //our simple custom  middlware
@@ -25,7 +28,13 @@ app.use((req, res, next) => {
 });
 app.use("/api/notes", notesRoutes); // api/notes repeted with all APIs so we make this standard API
 //app.use("/api/notes", productsRoutes); // make rotes seperated in there own fille make server.js more orgonized
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
+  });
+}
 const PORT = process.env.PORT;
 
 connectDB().then(() => {
